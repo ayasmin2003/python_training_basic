f75f8bbdd9fb125acebcf911c9508e44d4d6c3e6
Asma Yasmin <asma.yasmin@midaxo.com>
2019-03-04 15:23:35 +0200

    new python exercise and answer

---
+++ Python_expression_exercises-1.py
@@ -0,0 +1,15 @@
+#1
+number_feet= 5230*23
+print(number_feet  )
+print(" print it %s" %number_feet)
+print ('print it')
+print(number_feet)
+
+
+#2
+seconds= (10*60*60)+(35*60)+20
+print(seconds)
+
+#3
+var =2*10+2*15
+print(var)
