eddf69aa28d0a5f4baa4ecc8ac01d4301839d689
Asma Yasmin <asma.yasmin@midaxo.com>
2019-03-04 20:02:59 +0200

    string exercise 2

---
+++ string exercise 2.py
@@ -0,0 +1,44 @@
+#Playing with string
+
+#1.
+
+my_string = 'anomalocaris'
+result = my_string.index('o')
+print(result)
+
+#2
+result_r =my_string.index('r')
+print(result_r)
+
+#3
+my_string_p= 'waging on the purple drone'
+result_p = my_string_p.index('on')
+print (result_p)
+
+#4
+my_string_4 = 'Superficial'
+
+
+
+#5.
+
+my_long_string = "There truly is a dazzling bright world out there, waiting for us to explore."
+#find second half of the string
+second_half= my_long_string[(len(my_long_string))//2:]
+print (second_half)
+index_a_2nd_half = second_half.index('a')
+result = len(second_half)+index_a_2nd_half
+print(result)
+
+#
+
+#6
+#
+#
+# #7
+#
+# #8
+#
+#
+# #9
+
