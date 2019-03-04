632d2571f72a70d179db18f68992c868f72c0ab6
Asma Yasmin <asma.yasmin@midaxo.com>
2019-03-04 15:37:04 +0200

    new python exercise and answer

--- Python_expression_exercises-1.py
+++ Python_expression_exercises-1.py
@@ -13,3 +13,121 @@ print(seconds)
 #3
 var =2*10+2*15
 print(var)
+
+#4
+wh=10*25
+print(wh)
+#5.
+
+
+#1a.
+print(5280*23)
+#1b
+feet_in_mile= 5280
+total_mile=23
+total_length = feet_in_mile*total_mile
+print(total_length)
+print (("Length is ")+ str(feet_in_mile*total_mile))
+
+
+#2a.
+print(20+(35*60)+(10*60*60))
+#2b
+
+hours =10
+minutes = 35
+seconds=20
+total_seconds = hours*60*60+minutes*60+seconds
+print(total_seconds)
+
+
+
+#3a:
+print(2*10+2*15)
+
+#3b
+
+width =10
+height=15
+perimeter =width*2+height*2
+print (perimeter)
+#4a.
+print(10*25)
+
+width =10
+height=25
+area =width*height
+print (area)
+
+#5a.
+print(2*3.14*8)
+
+print('5b.')
+
+radius =8
+PI = 3.14159
+circumference =2*PI*radius
+print (circumference)
+
+#6a.
+print('6a. area of a circle')
+print(3.14159*8**2)
+
+print('6b.')
+print('6b. area of a circle')
+area=PI*radius**2
+print (area)
+
+print('7a.')
+print(1000*(1+0.01*7*10))
+
+print('7b.')
+present_value = 1000
+annual_rate = 7
+years = 10
+future_value = present_value*(1+0.01*annual_rate*years)
+print ('future value')
+print(future_value)
+
+
+print('8a.')
+print('My name is '+ 'John '+'Smith')
+
+print('8b.')
+first_name = 'John'
+last_name = 'Smith'
+print ('My name is %s %s' %(first_name, last_name))
+print ('My name is {} {}'.format(first_name, last_name))
+print ('My name is {1} {0}'.format(first_name, last_name))
+print ('My name is {first_name} {last_name}'.format(first_name='Laura', last_name=last_name))
+print ('following function in python 3.6')
+print(f'My name is {first_name} {last_name}')
+print(f'My name is {first_name} {last_name}')
+
+
+print('9a.')
+print('My name is John Smith. ' + 'I am '+ str(40)+' years old')
+
+print('9b.')
+name ='John Smith'
+age = '40' # as string
+age1= 40   # as integer
+
+print(f'My name is {name} and I am {age} years old')
+print('My name is %s and I am %s years old' %(name, age))
+print('My name is %s and I am %i years old' %(name, age1))
+
+
+#10a
+print(((2-5)**2+(2-6)**2)**0.5)
+
+
+print('10b.')
+x0 = 2
+x1 = 5
+y0 = 2
+y1 = 6
+
+results = ((x0-x1)**2+(y0-y1)**2)**.5
+print(results)
+
